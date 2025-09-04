8a9401c7f8295d332d5b90b540d89c8284306b96
Ridoy-Mojumder <hridoymoju463@gmail.com>
2025-09-04 13:37:40 +0600

    Create a basic stracture

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -12,16 +12,16 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/BUBT Nexus Logo Variations.png')}
           style={styles.reactLogo}
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Hello This is BUBT NEXUS Team!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle"></ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
@@ -67,7 +67,7 @@ const styles = StyleSheet.create({
   },
   reactLogo: {
     height: 178,
-    width: 290,
+    width: 320,
     bottom: 0,
     left: 0,
     position: 'absolute',
